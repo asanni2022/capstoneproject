afbbddea72a8a22cf62e737b5186bdaea7558245
asanni2022 <akeem.sql01@gmail.com>
2023-11-29 12:51:11 -0600

    added code

--- azpulumi_vmtemplate/__main__.py
+++ azpulumi_vmtemplate/__main__.py
@@ -1,174 +1,123 @@
 import pulumi
-from pulumi_azure_native import resources, network, compute
-from pulumi_random import random_string
-import base64
+import pulumi_azure as azure
+from pulumi_azure import core
 
-# Import the program's configuration settings.
-config = pulumi.Config()
-vm_name = config.get("vmName", "my-server")
-vm_size = config.get("vmSize", "Standard_A1_v2")
-os_image = config.get("osImage", "Debian:debian-11:11:latest")
-admin_username = config.get("adminUsername", "pulumiuser")
-service_port = config.get("servicePort", "80")
-ssh_public_key = config.require("sshPublicKey")
+# Create an Azure Resource Group
+resource_group = core.ResourceGroup('rg')
 
-os_image_publisher, os_image_offer, os_image_sku, os_image_version = os_image.split(":")
-
-# Create a resource group.
-resource_group = resources.ResourceGroup("resource-group")
-
-# Create a virtual network.
-virtual_network = network.VirtualNetwork(
-    "network",
+# Create a virtual network
+virtual_network = azure.network.VirtualNetwork('vnet',
     resource_group_name=resource_group.name,
-    address_space=network.AddressSpaceArgs(
-        address_prefixes=[
-            "10.0.0.0/16",
-        ],
-    ),
-    subnets=[
-        network.SubnetArgs(
-            name=f"{vm_name}-subnet",
-            address_prefix="10.0.1.0/24",
-        ),
-    ],
-)
-# Use a random string to give the VM a unique DNS name.
-domain_name_label = random_string.RandomString(
-    "domain-label",
-    length=8,
-    upper=False,
-    special=False,
-).result.apply(lambda result: f"{vm_name}-{result}")
+    address_spaces=['10.0.0.0/16'])
 
-# Create a public IP address for the VM.
-public_ip = network.PublicIPAddress(
-    "public-ip",
+# Create a public subnet
+public_subnet = azure.network.Subnet('public-subnet',
     resource_group_name=resource_group.name,
-    public_ip_allocation_method=network.IpAllocationMethod.DYNAMIC,
-    dns_settings=network.PublicIPAddressDnsSettingsArgs(
-        domain_name_label=domain_name_label,
-    ),
+    virtual_network_name=virtual_network.name,
+    address_prefix='10.0.1.0/24')
+
+# Create a private subnet
+private_subnet = azure.network.Subnet('private-subnet',
+    resource_group_name=resource_group.name,
+    virtual_network_name=virtual_network.name,
+    address_prefix='10.0.2.0/24')
+
+# Create a Bastion Host
+bastion_host = azure.network.BastionHost("bastion",
+    resource_group_name=resource_group.name,
+    location=resource_group.location,
+    subnet_id=azure.network.SubnetAssociation('association',
+        subnet_id=public_subnet.id,
+        bastion_host=bastion_host
+    ).id
 )
 
-# Create a security group allowing inbound access over ports 80 (for HTTP) and 22 (for SSH).
-security_group = network.NetworkSecurityGroup(
-    "security-group",
+# Create an Auto Scaling Group
+autoscale_setting = azure.monitoring.AutoscaleSetting("autoscale",
     resource_group_name=resource_group.name,
+    enabled=True,
+    target_resource_id=virtual_network.id,
+    profiles=[{
+        "name": "Auto1",
+        "capacity": {
+            "default": "1",
+            "maximum": "2",
+            "minimum": "1"
+        },
+        "rules": [{
+            "metric_trigger": {
+                "metric_name": "Percentage CPU",
+                "metric_resource_id": virtual_network.id,
+                "time_grain": "PT1M",
+                "statistic": "Average",
+                "time_window": "PT5M",
+                "time_aggregation": "Average",
+                "operator": "GreaterThanOrEqual",
+                "threshold": 75
+            },
+            "scale_action": {
+                "direction": "Increase",
+                "type": "ChangeCount",
+                "value": "1",
+                "cooldown": "PT1M"
+            }
+        }]
+    }]
+)
+
+# Create Azure Service Bus
+servicebus_namespace = azure.servicebus.Namespace("servicebus",
+    resource_group_name=resource_group.name,
+    location=resource_group.location
+)
+
+# Create Azure Application Insight
+app_insight = azure.appinsights.Insights("appinsight",
+    resource_group_name=resource_group.name,
+    location=resource_group.location,
+    application_type="web"
+)
+
+# Create Azure Key Vault
+key_vault = azure.keyvault.KeyVault("keyvault",
+    resource_group_name=resource_group.name,
+    location=resource_group.location,
+    tenant_id=azure.core.Tenant('tenant',
+        display_name='Ten').id,
+    sku_name="standard"
+)
+
+# Create Azure Network Security Group
+network_sg = azure.network.NetworkSecurityGroup("nsg",
+    resource_group_name=resource_group.name,
+    location=resource_group.location,
     security_rules=[
-        network.SecurityRuleArgs(
-            name=f"{vm_name}-securityrule",
-            priority=1000,
-            direction=network.AccessRuleDirection.INBOUND,
+        azure.network.NetworkSecurityGroupSecurityRuleArgs(
+            name="SSH",
+            priority=1001,
+            direction="Inbound",
             access="Allow",
             protocol="Tcp",
             source_port_range="*",
-            source_address_prefix="*",
-            destination_address_prefix="*",
-            destination_port_ranges=[
-                service_port,
-                "22",
-            ],
+            source_address_prefix="Internet",
+            destination_port_range=[22],
+            destination_address_prefix="*"
         ),
-    ],
+    ]
 )
 
-# Create a network interface with the virtual network, IP address, and security group.
-network_interface = network.NetworkInterface(
-    "network-interface",
+# Create a Postgres DB
+database_account = azure.postgresql.Server('pg-server',
     resource_group_name=resource_group.name,
-    network_security_group=network.NetworkSecurityGroupArgs(
-        id=security_group.id,
-    ),
-    ip_configurations=[
-        network.NetworkInterfaceIPConfigurationArgs(
-            name=f"{vm_name}-ipconfiguration",
-            private_ip_allocation_method=network.IpAllocationMethod.DYNAMIC,
-            subnet=network.SubnetArgs(
-                id=virtual_network.subnets.apply(lambda subnets: subnets[0].id),
-            ),
-            public_ip_address=network.PublicIPAddressArgs(
-                id=public_ip.id,
-            ),
-        ),
-    ],
+    location=resource_group.location,
+    sku_name="B_Gen5_2",
+    administrator_login="adminlogin",
+    administrator_login_password="adminpassword",
+    ssl_enforcement="Enabled",
+    storage_mb=51200,
+    version="9.5"
 )
 
-# Define a script to be run when the VM starts up.
-init_script = f"""#!/bin/bash
-    echo '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Hello, world!</title>
-    </head>
-    <body>
-        <h1>Hello, world! 👋</h1>
-        <p>Deployed with 💜 by <a href="https://pulumi.com/">Pulumi</a>.</p>
-    </body>
-    </html>' > index.html
-    sudo python3 -m http.server {service_port} &
-    """
+# Export the connection string for the storage account
+pulumi.export('connection_string', database_account.connection_strings[0])
 
-# Create the virtual machine.
-vm = compute.VirtualMachine(
-    "vm",
-    resource_group_name=resource_group.name,
-    network_profile=compute.NetworkProfileArgs(
-        network_interfaces=[
-            compute.NetworkInterfaceReferenceArgs(
-                id=network_interface.id,
-                primary=True,
-            )
-        ]
-    ),
-    hardware_profile=compute.HardwareProfileArgs(
-        vm_size=vm_size,
-    ),
-    os_profile=compute.OSProfileArgs(
-        computer_name=vm_name,
-        admin_username=admin_username,
-        custom_data=base64.b64encode(bytes(init_script, "utf-8")).decode("utf-8"),
-        linux_configuration=compute.LinuxConfigurationArgs(
-            disable_password_authentication=True,
-            ssh=compute.SshConfigurationArgs(
-                public_keys=[
-                    compute.SshPublicKeyArgs(
-                        key_data=ssh_public_key,
-                        path=f"/home/{admin_username}/.ssh/authorized_keys",
-                    ),
-                ],
-            ),
-        ),
-    ),
-    storage_profile=compute.StorageProfileArgs(
-        os_disk=compute.OSDiskArgs(
-            name=f"{vm_name}-osdisk",
-            create_option=compute.DiskCreateOption.FROM_IMAGE,
-        ),
-        image_reference=compute.ImageReferenceArgs(
-            publisher=os_image_publisher,
-            offer=os_image_offer,
-            sku=os_image_sku,
-            version=os_image_version,
-        ),
-    ),
-)
-
-# Once the machine is created, fetch its IP address and DNS hostname.
-vm_address = vm.id.apply(
-    lambda id: network.get_public_ip_address_output(
-        resource_group_name=resource_group.name,
-        public_ip_address_name=public_ip.name,
-    )
-)
-
-# Export the VM's hostname, public IP address, and HTTP URL.
-pulumi.export("ip", vm_address.ip_address)
-pulumi.export("hostname", vm_address.dns_settings.apply(lambda settings: settings.fqdn))
-pulumi.export(
-    "url",
-    vm_address.dns_settings.apply(
-        lambda settings: f"http://{settings.fqdn}:{service_port}"
-    ),
-)
